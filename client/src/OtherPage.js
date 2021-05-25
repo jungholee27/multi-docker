f86cea8aaad6b281e46bc9bfc99cfcfe5892bef5
Jung Ho Lee <jungholee@Jungs-MacBook-Pro.local>
2021-05-25 16:09:53 -0400

    repo fix

---
+++ client/src/OtherPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const OtherPage = () => {
+  return (
+    <div>
+      DANKDANKDANK
+      <Link to ="/">GO HOME</Link>
+    </div>
+  );
+}
+
+export default OtherPage;
